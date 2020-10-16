a3579b1885b12413434e3e596d5dab29ee4b0f61
tsakunova <tv.tsakunova@gmail.com>
2020-10-16 23:28:07 +0300

    fix recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,12 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  constructor() {
+  calculateDepth(arr) {
+    let counter = 1
+    let localCounter = 0
+    arr.forEach(element=>{
+      if (Array.isArray(element)){
+        const depth = this.calculateDepth(element)
+        if (localCounter < depth) {
+          localCounter = depth
+        }
+      }
+    })
+    counter = counter + localCounter
+    return counter
+  }
+  /* constructor() {
     this.counter = 1;
-    this.localCounter = 1;
+    this.localCounter = 0;
   }
   calculateDepth(arr) {
-    for(let i = 0; i < arr.length; i ++) {
+
+   for(let i = 0; i < arr.length; i ++) {
       if (Array.isArray(arr[i])) {
         this.localCounter++;
         this.calculateDepth(arr[i])
@@ -17,4 +32,5 @@ module.exports = class DepthCalculator {
     }
     return this.counter;
   }
+  */
 };

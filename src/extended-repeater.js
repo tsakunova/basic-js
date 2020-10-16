b1434d8831f71c585efc1b46b3be851e810e6c45
tsakunova <tv.tsakunova@gmail.com>
2020-10-16 21:51:20 +0300

    done extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, {repeatTimes,
+  separator = '+',
+  addition = '',
+  additionRepeatTimes,
+  additionSeparator = '|'}) {
+
+let newStr = ''
+let add = String(addition)
+
+  let addStr = (add+additionSeparator).repeat(additionRepeatTimes-1)+add;
+newStr = str+addStr+separator;
+return newStr.repeat(repeatTimes-1)+str+addStr;
+
 };
-  
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.counter = 1;
+    this.localCounter = 1;
+  }
+  calculateDepth(arr) {
+    for(let i = 0; i < arr.length; i ++) {
+      if (Array.isArray(arr[i])) {
+        this.localCounter++;
+        this.calculateDepth(arr[i])
+        if (this.localCounter > this.counter) {
+          this.counter = this.localCounter
+        }
+      }
+    }
+    return this.counter;
   }
 };

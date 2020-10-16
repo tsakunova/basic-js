ffe6fb2e3f43d40d0c7e762e5fdb8062e34ba95f
tsakunova <tv.tsakunova@gmail.com>
2020-10-17 00:02:08 +0300

    done carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,10 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  const parse = parseFloat(sampleActivity)
+  if(parse>15 || parse<=0 || !sampleActivity || typeof sampleActivity!= 'string'||arguments.length ===0||isNaN(parse)){return false}
+  let a = 0.693 /HALF_LIFE_PERIOD
+  let age = Math.ceil(Math.log(MODERN_ACTIVITY /parse) / a)
+  return age
 };
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,9 +2,11 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
-    let counter = 1
-    let localCounter = 0
-    arr.forEach(element=>{
+    let counter = 1 // массив
+    let localCounter = 0 // массив внутри
+    // пробежим по каждому элементу
+    arr.forEach(element=>{ 
+     // проверим является ли элемент массивом
       if (Array.isArray(element)){
         const depth = this.calculateDepth(element)
         if (localCounter < depth) {
@@ -15,22 +17,5 @@ module.exports = class DepthCalculator {
     counter = counter + localCounter
     return counter
   }
-  /* constructor() {
-    this.counter = 1;
-    this.localCounter = 0;
-  }
-  calculateDepth(arr) {
 
-   for(let i = 0; i < arr.length; i ++) {
-      if (Array.isArray(arr[i])) {
-        this.localCounter++;
-        this.calculateDepth(arr[i])
-        if (this.localCounter > this.counter) {
-          this.counter = this.localCounter
-        }
-      }
-    }
-    return this.counter;
-  }
-  */
 };
